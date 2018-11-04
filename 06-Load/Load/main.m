8ab2afa12839a5bb5384fc00605d0add64a46971
chenlong11 <chenlong11@baidu.com>
2018-11-04 21:08:32 +0800

    add 06 load project

---
+++ 06-Load/Load/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Load
+//
+//  Created by Csy on 2018/11/3.
+//  Copyright © 2018 Csy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
